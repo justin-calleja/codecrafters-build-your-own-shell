e7d6eedd71abe3e55fca77bea35c199781e5e0ae
Justin <calleja.justin@gmail.com>
2026-01-02 20:49:40 +0100

    echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cctype>
+
+std::string &ltrim(std::string &s)
+{
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch)
+                                  { return !std::isspace(ch); }));
+  return s;
+}
+
+std::string &rtrim(std::string &s)
+{
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch)
+                       { return !std::isspace(ch); })
+              .base(),
+          s.end());
+  return s;
+}
+
+std::string &trim(std::string &s)
+{
+  return ltrim(rtrim(s));
+}
 
 int main()
 {
@@ -15,9 +38,19 @@ int main()
 
     std::string command;
     std::getline(std::cin, command);
+    trim(command);
 
-    if (command == "exit") {
+    if (command == "exit")
+    {
       return 0;
+    }
+
+    if (command.rfind("echo ", 0) == 0)
+    {
+      std::string echoCommand = command.substr(4);
+      trim(echoCommand);
+      std::cout << echoCommand << std::endl;
+      continue;
     }
 
     std::cout << command << ": command not found" << std::endl;
