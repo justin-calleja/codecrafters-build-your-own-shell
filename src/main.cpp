ee0b74c4e0b82aaeba4fc89e2b1b24219c03d5b8
Justin <calleja.justin@gmail.com>
2026-01-02 18:37:11 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -8,4 +8,9 @@ int main()
   std::cerr << std::unitbuf;
 
   std::cout << "$ ";
+
+  std::string command;
+  std::getline(std::cin, command);
+
+  std::cout << command << ": command not found" << std::endl;
 }
