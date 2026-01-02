4e5dbe7a8d46e53842578d0ed0a107546f207b71
Justin <calleja.justin@gmail.com>
2026-01-02 20:22:12 +0100

    exit

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,18 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  while (true)
+  bool isLoopEnabled = true;
+
+  while (isLoopEnabled)
   {
     std::cout << "$ ";
 
     std::string command;
     std::getline(std::cin, command);
+
+    if (command == "exit") {
+      return 0;
+    }
 
     std::cout << command << ": command not found" << std::endl;
   }
